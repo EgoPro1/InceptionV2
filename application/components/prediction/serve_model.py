283c2d745223bc19deb8a6b74413c97fe4f4d018
Joaquin <joaquinegocheagaramos@gmail.com>
2021-09-04 09:37:11 -0500

    inceptionv2

--- application/components/prediction/serve_model.py
+++ application/components/prediction/serve_model.py
@@ -3,35 +3,47 @@ from io import BytesIO
 import numpy as np
 import tensorflow as tf
 from PIL import Image
-from tensorflow.keras.applications.imagenet_utils import decode_predictions
+from tensorflow.python.keras.applications.imagenet_utils import decode_predictions
+from tensorflow.python.keras.applications.inception_resnet_v2 import InceptionResNetV2             
+from tensorflow.python.keras.preprocessing import image as imgx                                          
+from tensorflow.python.keras.applications.inception_resnet_v2 import preprocess_input, decode_predictions
 
 model = None
 
 
 def load_model():
     model = tf.keras.applications.MobileNetV2(weights="imagenet")
-    print("Model loaded")
-    return model
+    model1=InceptionResNetV2(weights='imagenet')
+    if model1 is None:
+        print("Model1 NOT loaded")
+    else:
+        print("Model1  loaded")
+    print("Model loaded")    
+    return model1
 
 
-def predict(image: Image.Image):
+def predict(img: Image.Image):
     global model
     if model is None:
         model = load_model()
-
-    image = np.asarray(image.resize((224, 224)))[..., :3]
-    image = np.expand_dims(image, 0)
-    image = image / 127.5 - 1.0
-
-    result = decode_predictions(model.predict(image), 2)[0]
+    print("imagen ")
+    print(img)
+    img = np.asarray(img.resize((299, 299)))[..., :3]
+    x = imgx.img_to_array(img)                                                      
+    x = np.expand_dims(x, axis=0)                                                    
+    x = preprocess_input(x)                                                          
+                                                                                 
+    preds = model.predict(x)                                                         
+    print ('Prediction:', decode_predictions(preds, top=1)[0][0])
+    res= decode_predictions(preds, top=1)[0][0]
 
     response = []
-    for i, res in enumerate(result):
-        resp = {}
-        resp["class"] = res[1]
-        resp["confidence"] = f"{res[2]*100:0.2f} %"
+    
+    resp = {}
+    resp["class"] = res[1]
+    resp["confidence"] = f"{res[2]*100:0.2f} %"
 
-        response.append(resp)
+    response.append(resp)
 
     return response
 
--- application/server/main.py
+++ application/server/main.py
@@ -1,7 +1,9 @@
 import uvicorn
-from fastapi import FastAPI, File, UploadFile
+from fastapi import (FastAPI, File, UploadFile)
 from starlette.responses import RedirectResponse
-
+from tensorflow.python.keras.preprocessing import image as imgx
+import requests
+from PIL import Image
 from application.components import predict, read_imagefile
 from application.schema import Symptom
 from application.components.prediction import symptom_check
@@ -19,15 +21,20 @@ async def index():
 
 
 @app.post("/predict/image")
-async def predict_api(file: UploadFile = File(...)):
-    extension = file.filename.split(".")[-1] in ("jpg", "jpeg", "png")
+async def predict_api(filename):
+    extension = filename.split(".")[-1] in ("jpg", "jpeg", "png")
+ 
+    print(filename)
+    print(extension)
     if not extension:
         return "Image must be jpg or png format!"
-    image = read_imagefile(await file.read())
+
+    image = Image.open(requests.get(filename, stream=True).raw)
+    
+         
     prediction = predict(image)
 
     return prediction
-
 
 @app.post("/api/covid-symptom-check")
 def check_risk(symptom: Symptom):
