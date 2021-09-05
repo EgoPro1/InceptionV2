783099e27055583e7c9f2c8f24e99161f1405d92
Joaquin <joaquinegocheagaramos@gmail.com>
2021-09-04 22:58:28 -0500

    last version

--- application/server/main.py
+++ application/server/main.py
@@ -7,13 +7,15 @@ from PIL import Image
 from application.components import predict, read_imagefile
 from application.schema import Symptom
 from application.components.prediction import symptom_check
+from googletrans import Translator, constants
+from pprint import pprint
 
 app_desc = """<h2>Try this app by uploading any image with `predict/image`</h2>
 <h2>Analize photos</h2>
 <br>Template by Aniket Maurya, new version by Joaquin Egocheaga"""
 
 app = FastAPI(title='Comparizy  ,  Tensorflow FastAPI ', description=app_desc)
-
+translator = Translator()
 
 @app.get("/", include_in_schema=False)
 async def index():
@@ -21,20 +23,25 @@ async def index():
 
 
 @app.post("/predict/image")
-async def predict_api(filename):
-    extension = filename.split(".")[-1] in ("jpg", "jpeg", "png")
- 
-    print(filename)
+async def predict_api(file: UploadFile = File(...)):
+    extension = file.filename.split(".")[-1] in ("jpg", "jpeg", "png")
+    print(file.filename)
     print(extension)
     if not extension:
         return "Image must be jpg or png format!"
 
-    image = Image.open(requests.get(filename, stream=True).raw)
-    
-         
+    image = read_imagefile(await file.read())
     prediction = predict(image)
-
-    return prediction
+    clase=prediction[0]['class']
+    clase=clase.replace("_", " ")
+    print(clase)
+    
+    print("X")
+    translation = translator.translate(clase, "es") 
+    translation=translation.text
+    print(translation)
+    
+    return translation
 
 @app.post("/api/covid-symptom-check")
 def check_risk(symptom: Symptom):
