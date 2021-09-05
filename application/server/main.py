ae087a0125819b8aa506f267c60c796a1d6c7133
Joaquin <joaquinegocheagaramos@gmail.com>
2021-09-04 23:21:45 -0500

    prueba

--- application/server/main.py
+++ application/server/main.py
@@ -7,15 +7,13 @@ from PIL import Image
 from application.components import predict, read_imagefile
 from application.schema import Symptom
 from application.components.prediction import symptom_check
-from googletrans import Translator, constants
-from pprint import pprint
 
 app_desc = """<h2>Try this app by uploading any image with `predict/image`</h2>
 <h2>Analize photos</h2>
 <br>Template by Aniket Maurya, new version by Joaquin Egocheaga"""
 
 app = FastAPI(title='Comparizy  ,  Tensorflow FastAPI ', description=app_desc)
-translator = Translator()
+
 
 @app.get("/", include_in_schema=False)
 async def index():
@@ -35,16 +33,8 @@ async def predict_api(filename):
     
          
     prediction = predict(image)
-    clase=prediction[0]['class']
-    clase=clase.replace("_", " ")
-    print(clase)
-    
-    print("X")
-    translation = translator.translate(clase, "es") 
-    translation=translation.text
-    print(translation)
-    
-    return translation
+
+    return prediction
 
 @app.post("/api/covid-symptom-check")
 def check_risk(symptom: Symptom):
